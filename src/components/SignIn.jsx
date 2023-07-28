ebf0a59943fbd24854a489460aee306b6b653786
Jordan-Rob <jordanrob709@gmail.com>
2023-07-28 11:40:29 +0300

    Exercise 10.8: the sign-in form

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,35 @@
+import { View, StyleSheet, Pressable } from 'react-native';
+import { Formik } from 'formik';
+import FormikTextInput from './FormikTextInput';
 import Text from './Text';
 
+const initialValues = {
+    mass: '',
+    height: '',
+};
+
+const SignInForm = ({onSubmit}) => {
+    return (
+        <View>
+            <FormikTextInput name={"Username"} placeholder={"username"}></FormikTextInput>
+            <FormikTextInput name={"Password"} placeholder={"password"}></FormikTextInput>
+            <Pressable onPress={onSubmit}>
+              <Text>Sign In</Text>
+            </Pressable>
+        </View>
+    )
+}
+
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  )
 };
 
 export default SignIn;
